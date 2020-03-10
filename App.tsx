502813f8c2d95f23db05a3cdd5f05b95a4fef617
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-10 16:24:02 +0700

    add react-native-elements

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider } from "react-native-elements";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    // </View>
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
