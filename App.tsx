a979001dae924ba79aa9f735e2da519eff822c10
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-13 20:56:45 +0700

    add safe area

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-// import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from "react-native";
+import {
+    SafeAreaProvider,
+    SafeAreaView,
+    initialWindowSafeAreaInsets,
+} from "react-native-safe-area-context";
 import { Button, ThemeProvider } from "react-native-elements";
 
 export default function App() {
     return (
-        // <View style={styles.container}>
-        //   <Text>Open up App.tsx to start working on your app!</Text>
-        // </View>
-        <ThemeProvider>
-            <Button title="Heyooo!" />
-        </ThemeProvider>
+        <SafeAreaProvider initialSafeAreaInsets={initialWindowSafeAreaInsets}>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+                <ThemeProvider></ThemeProvider>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
