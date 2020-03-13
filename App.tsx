3069c2144240526a23f3dfd732e8e12bc8cce3c5
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-13 21:29:24 +0700

    update screens

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "react-native-gesture-handler";
-import { StatusBar, View } from "react-native";
+import { StatusBar, View, Text, Button } from "react-native";
 import {
     SafeAreaProvider,
     SafeAreaView,
@@ -10,7 +10,24 @@ import { ThemeProvider } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import ErrorBoundary from "./app/components/errorBoundary";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./app/screens/Login";
+
+function HomeScreen({ navigation }) {
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate("Login")}
+            />
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
@@ -19,9 +36,14 @@ export default function App() {
                 <StatusBar barStyle="dark-content" />
                 <NavigationContainer>
                     <ErrorBoundary>
-                        <SafeAreaView>
-                            <Login />
-                        </SafeAreaView>
+                        <Stack.Navigator>
+                            <Stack.Screen
+                                name="Home"
+                                component={HomeScreen}
+                                options={{ title: "Overview" }}
+                            />
+                            <Stack.Screen name="Login" component={Login} />
+                        </Stack.Navigator>
                     </ErrorBoundary>
                 </NavigationContainer>
             </ThemeProvider>
