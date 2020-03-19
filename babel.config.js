931e58c1ee2113cf3feb521cb8b29f9ad11aff1e
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-19 23:03:40 +0700

    add date-fns + babel

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = function(api) {
             ],
             "@babel/plugin-proposal-export-default-from",
             "@babel/plugin-proposal-export-namespace-from",
+            "date-fns",
         ],
     };
 };
