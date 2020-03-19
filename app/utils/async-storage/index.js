eca8584f8b9e12e3af3cf9af0a149c075fb2b9c4
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-19 22:17:14 +0700

    update async

---
+++ app/utils/async-storage/index.js
@@ -0,0 +1,7 @@
+export clear from "./clear";
+export flushGetRequests from "./flushGetRequests";
+export getAllKeys from "./getAllKeys";
+export getItem from "./getItem";
+export mergeItem from "./mergeItem";
+export removeItem from "./removeItem";
+export setItem from "./setItem";
