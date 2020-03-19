deb4530081715d25370a9df3f8f8f4f030012653
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-20 00:58:12 +0700

    update config + endpoints

---
+++ app/endpoints.js
@@ -0,0 +1,49 @@
+import config from "./config";
+
+export const LOGIN = config.api_url + "auth/login";
+
+export const ME = config.api_url + "me";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
+
+// export const LOGIN = config.api_url + "/auth/login";
