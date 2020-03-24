4772375aa44e18dffadaea42cb5f9bd56ce02851
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-03-25 03:11:57 +0700

    push notification

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,15 +4,34 @@ import {
   BottomNavigationTab,
   Icon,
 } from '@ui-kitten/components';
-import React from 'react';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 
+import { NOTIS } from '../endpoints';
+import { Put } from '../utils/api_caller';
 import Group from './Group';
 import QuickView from './Notification';
 import Profile from './Profile';
 import Task from './Task';
 
 const Tab = createBottomTabNavigator();
+
+async function registerForPushNotificationsAsync() {
+  const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+  if (status !== 'granted') {
+    alert('No notification permissions!');
+    return;
+  }
+
+  const token = await Notifications.getExpoPushTokenAsync();
+  Put({
+    to: NOTIS,
+    params: { token: token.replace('ExponentPushToken[', '').replace(']', '') },
+  });
+}
 
 const BottomTabBar = ({ navigation, state }) => {
   const onSelect = index => {
@@ -44,6 +63,9 @@ const BottomTabBar = ({ navigation, state }) => {
 };
 
 export default function Home() {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
   return (
     <Tab.Navigator tabBar={props => <BottomTabBar {...props} />}>
       <Tab.Screen name="Group" component={Group} />
